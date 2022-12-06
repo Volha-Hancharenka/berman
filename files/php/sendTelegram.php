8840de5c209c1b63a22f13f024401fb832b5326e
Volha-Hancharenka <goncharenkolga4567@gmail.com>
2022-12-06 14:56:20 +0300

    Updates

---
+++ files/php/sendTelegram.php
@@ -0,0 +1,38 @@
+<?php
+  $token          = "5932511387:AAGDS1JojBr3fUwi3mcubhBeFGNtPQ3e6Jw";
+  $chat_id        = "-678847938";
+
+  $name           = $_POST['user_name'];
+  $phone          = $_POST['user_phone'];
+  $email          = $_POST['user_email'];
+  $problems       = $_POST['problems'];
+  $emotions       = $_POST['emotions'];
+  $cituations     = $_POST['cituations'];
+
+  $arr = array(
+    'Имя: ' => $name,
+    'Телефон: ' => $phone,
+    'E-mail: ' => $email,
+    'Проблема клиента: ' => $problems,
+    'Эмоции клиента: ' => $emotions,
+    'Ситуация клиента: ' => $cituations,
+  );
+
+  foreach($arr as $key => $value) {
+    $txt .= "<b>".$key."</b> ".$value."%0A";
+  };
+
+  $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+  if ($sendToTelegram) {
+    $message = "Заявка успешно отправлена!";
+    $status = 200;
+  } else {
+    $message = "Error";
+    $status = 500;
+    throw new Exception();
+  }
+
+  header('Content-Type: application/json');
+  echo json_encode(["message" => $message, "status" => $status]);
+?>

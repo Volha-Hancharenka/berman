380678f4ff6ec8f671a4cc665075be0a5d3fbba9
Сергей <serj.elessar@gmail.com>
2022-12-06 23:54:53 +0300

    feat: create data media

---
+++ src/js/helpers/mediaData.js
@@ -0,0 +1,26 @@
+const mediaData = {
+  audio: [
+    {
+      id: '1',
+      name: 'implementation-of-goals',
+      secure: 'goal',
+    },
+    {
+      id: '2',
+      name: 'difficult-situation',
+      secure: 'answer',
+    },
+    {
+      id: '3',
+      name: 'recuperation',
+      secure: 'protection',
+    },
+    {
+      id: '4',
+      name: 'for-couples',
+      secure: 'couple',
+    },
+  ]
+}
+
+export default mediaData
